90a45607a5af45357b1ac32b0292e6992df247bf
Swaroop <swaroopcharan1712@gmail.com>
2026-07-03 16:59:07 +0530

    Added the fitness calculator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 
 class Rocket:
@@ -8,8 +9,17 @@ class Rocket:
         self.y = y
         self.vx = 0
         self.vy = 0
-        self.dna = DNA(600)
+        self.dna = DNA(300)
         self.frame=0
+        self.fitness=0
+        
+    def calculate_fitness(self,target_x,target_y):
+        distance = math.sqrt((target_x-self.x)**2+(target_y-self.y)**2)
+        if distance == 0:
+            self.fitness = float('inf')
+            
+        else:
+            self.fitness = 1/distance
         
     def update(self):
         if self.frame < len(self.dna.genes):
@@ -46,14 +56,23 @@ class Population:
     def draw(self,surface):
         for rocket in self.rockets:
             rocket.draw(surface)
+            
+    def evaluate(self,target_x,target_y):
+        for rocket in self.rockets:
+            rocket.calculate_fitness(target_x,target_y)
+            
+    def all_done(self):
+        for rocket in self.rockets:
+            if rocket.frame < len(rocket.dna.genes):
+                return False
+        
+        return True
         
 WIDTH = 600
 HEIGHT = 600
 
 target_x,target_y = 300,50
 population = Population(50)
-
-rocket = Rocket(300,500)
 
 surface = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Rocket GA')
@@ -69,6 +88,9 @@ while running:
             running = False
             
     population.update()
+    
+    if population.all_done():
+        population.evaluate(target_x,target_y)
             
     surface.fill(color)
     pygame.draw.circle(surface,"red",(target_x,target_y),10)
